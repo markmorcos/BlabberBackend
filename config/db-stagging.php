5f7c529b42ceb97a337476affea79d1e21f0dbb0
Unknown <mahmoud.gamal.mousa@gmail.com>
2017-03-21 16:57:18 +0800

    remove unused db-stagging file

--- config/db-stagging.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=blabber_stagging',
-    'username' => 'blabber_user',
-    'password' => 'MMiP7gRIAFEX',
-    'charset' => 'utf8',
-];
