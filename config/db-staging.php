f33b70b3dae2375df1a7be4918e6134aca97d5de
Unknown <mahmoud.gamal.mousa@gmail.com>
2017-03-21 16:31:01 +0800

    fix staging database name

--- config/db-staging.php
+++ config/db-staging.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=blabber_stagging',
+    'dsn' => 'mysql:host=localhost;dbname=blabber_staging',
     'username' => 'blabber_user',
     'password' => 'MMiP7gRIAFEX',
     'charset' => 'utf8',
