707c50bdc383f5637b5d6dde00e3709c46330488
Unknown <mahmoud.gamal.mousa@gmail.com>
2017-03-27 23:17:28 +0800

    refactore if cond. to reverse if and code format for ApiBaseController

--- controllers/ApiBaseController.php
+++ controllers/ApiBaseController.php
@@ -2,60 +2,47 @@
 
 namespace app\controllers;
 
-use Yii;
-use yii\db\Query;
-use yii\web\Controller;
-use yii\web\UploadedFile;
-use yii\web\HttpException;
-use yii\helpers\Url;
-use yii\helpers\ArrayHelper;
-use app\models\User;
-use app\models\Friendship;
-use app\models\Category;
 use app\models\Business;
-use app\models\Country;
-use app\models\City;
-use app\models\Flag;
-use app\models\Interest;
-use app\models\BusinessFlag;
-use app\models\BusinessInterest;
-use app\models\UserFlag;
-use app\models\UserInterest;
-use app\models\SavedBusiness;
-use app\models\Checkin;
-use app\models\Review;
-use app\models\Media;
 use app\models\BusinessView;
-use app\models\Sponsor;
-use app\models\Report;
-use app\models\Comment;
+use app\models\Category;
+use app\models\Checkin;
+use app\models\Friendship;
+use app\models\Media;
 use app\models\Notification;
+use app\models\Review;
+use app\models\SavedBusiness;
+use app\models\User;
+use Yii;
 use yii\data\ActiveDataProvider;
+use yii\helpers\Url;
+use yii\web\Controller;
+use yii\web\HttpException;
+use yii\web\UploadedFile;
 
 class ApiBaseController extends Controller
 {
-    var $output = ['status' => 0, 'errors' => null];
-    var $logged_user = ['id'=>''];
-    var $pagination = [
+    public $output = ['status' => 0, 'errors' => null];
+    public $logged_user = ['id' => ''];
+    public $pagination = [
         'page_no' => null,
         'no_per_page' => 20,
-        'total_pages_no' => null        
+        'total_pages_no' => null,
     ];
-    var $adminEmail = 'admin@myblabber.com';
+    public $adminEmail = 'admin@myblabber.com';
 
     public function beforeAction($action)
     {
         // TODO check if this needed on live server
         $this->enableCsrfValidation = false;
 
-        $guest_actions = ['error', 'is-unique-username', 'sign-up', 'sign-in-fb', 'sign-in', 'recover-password', 
-            'get-profile', 'get-categories', 'get-sub-categories', 'get-countries', 'get-cities', 'get-flags', 'get-interests', 
-            'get-homescreen-businesses', 'get-businesses', 'search-businesses', 'search-businesses-by-type', 'get-business-data', 
-            'get-checkins', 'get-reviews', 'get-homescreen-reviews', 'get-media', 'get-homescreen-images', 'get-sponsors'
+        $guest_actions = ['error', 'is-unique-username', 'sign-up', 'sign-in-fb', 'sign-in', 'recover-password',
+            'get-profile', 'get-categories', 'get-sub-categories', 'get-countries', 'get-cities', 'get-flags', 'get-interests',
+            'get-homescreen-businesses', 'get-businesses', 'search-businesses', 'search-businesses-by-type', 'get-business-data',
+            'get-checkins', 'get-reviews', 'get-homescreen-reviews', 'get-media', 'get-homescreen-images', 'get-sponsors',
         ];
 
-        if( !$this->_verifyUserAndSetID() && !in_array($action->id, $guest_actions) ){
-            throw new HttpException(200, 'no valid user credentials input');
+        if (!$this->_verifyUserAndSetID() && !in_array($action->id, $guest_actions)) {
+            throw new HttpException(200, 'no valid user credentials input, if logged in try to logout and login again');
         }
 
         return parent::beforeAction($action);
@@ -66,16 +53,16 @@ class ApiBaseController extends Controller
         // Extract the params from the request and bind them to params
         $params = \yii\helpers\BaseArrayHelper::merge(Yii::$app->getRequest()->getBodyParams(), $params);
 
-        if( !empty($params['page']) ){
+        if (!empty($params['page'])) {
             $this->pagination['page_no'] = intval($params['page']);
         }
-        
+
         return parent::runAction($id, $params);
     }
 
     public function afterAction($action, $result)
     {
-        if( $this->pagination['page_no'] !== null ){
+        if ($this->pagination['page_no'] !== null) {
             $this->output['pagination'] = [
                 'page_no' => $this->pagination['page_no'],
                 'total_pages_no' => $this->pagination['total_pages_no'],
@@ -91,13 +78,12 @@ class ApiBaseController extends Controller
     {
         $exception = Yii::$app->errorHandler->exception;
 
-        if ($exception !== null)
-        {
+        if ($exception !== null) {
             $this->output['status'] = 1;
             $this->output['errors'] = $exception->getMessage();
         }
     }
-    
+
     /****************************************/
     /****************************************/
     /****************************************/
@@ -114,7 +100,7 @@ class ApiBaseController extends Controller
         $errors = '';
         foreach ($model->errors as $key => $element) {
             foreach ($element as $key => $error) {
-                $errors .= $error.', ';
+                $errors .= $error . ', ';
             }
         }
 
@@ -123,16 +109,16 @@ class ApiBaseController extends Controller
 
     protected function _verifyUserAndSetID()
     {
-        if( empty($_POST['user_id']) || empty($_POST['auth_key']) ){
+        if (empty($_POST['user_id']) || empty($_POST['auth_key'])) {
             return false;
         }
 
         $user = User::findOne($_POST['user_id']);
 
-        if( isset($user) && $user->auth_key == $_POST['auth_key'] ){
+        if (isset($user) && $user->auth_key == $_POST['auth_key']) {
             $this->logged_user = $user;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -140,19 +126,21 @@ class ApiBaseController extends Controller
     protected function _login($email, $password, $firebase_token)
     {
         $user = User::login($email, $password, $firebase_token);
-        if( $user !== null ){
-            if( $user->approved === 0 ){ //false
-                throw new HttpException(200, 'your account not approved yet, we will update you by email when it\'s done');
-            }
-            if( $user->blocked === 1 ){ //true
-                throw new HttpException(200, 'your account is blocked, please contact the support');
-            }
-
-            $this->output['user_data'] = $this->_getUserData($user);
-            $this->output['auth_key'] = $user->auth_key;
-        }else{
+        if ($user === null) {
             throw new HttpException(200, 'login problem');
         }
+
+        if ($user->approved === 0) {
+            //false
+            throw new HttpException(200, 'your account not approved yet, we will update you by email when it\'s done');
+        }
+        if ($user->blocked === 1) {
+            //true
+            throw new HttpException(200, 'your account is blocked, please contact the support');
+        }
+
+        $this->output['user_data'] = $this->_getUserData($user);
+        $this->output['auth_key'] = $user->auth_key;
     }
 
     protected function _getModelWithPagination($query)
@@ -165,7 +153,7 @@ class ApiBaseController extends Controller
             'query' => $query,
             'pagination' => [
                 'pageSize' => $this->pagination['no_per_page'],
-                'page' => $this->pagination['page_no']-1 // to make it zero based
+                'page' => $this->pagination['page_no'] - 1, // to make it zero based
             ],
         ]);
 
@@ -177,13 +165,13 @@ class ApiBaseController extends Controller
 
     protected function _validateUsername($username)
     {
-        if( strpos($username, ' ') !== false ){
+        if (strpos($username, ' ') !== false) {
             throw new HttpException(200, 'username can\'t contains spaces');
         }
 
         $model = User::find()
-                ->where(['username' => $username])
-                ->one();
+            ->where(['username' => $username])
+            ->one();
         if (!empty($model)) {
             throw new HttpException(200, 'this username already taken');
         }
@@ -191,10 +179,10 @@ class ApiBaseController extends Controller
 
     protected function _getUserData($user)
     {
-        if(empty($user)){
+        if (empty($user)) {
             return null;
         }
-        
+
         $user_data['id'] = $user->id;
         $user_data['name'] = $user->name;
         $user_data['email'] = $user->email;
@@ -205,19 +193,19 @@ class ApiBaseController extends Controller
         $user_data['interests'] = $user->interestsList;
 
         $last_sent_friend_request = $this->_getLastFriendshipRequest($this->logged_user['id'], $user->id);
-        $user_data['last_sent_friend_request'] = $last_sent_friend_request !== null ? $last_sent_friend_request->attributes : null ;
+        $user_data['last_sent_friend_request'] = $last_sent_friend_request !== null ? $last_sent_friend_request->attributes : null;
 
         $last_received_friend_request = $this->_getLastFriendshipRequest($user->id, $this->logged_user['id']);
-        $user_data['last_received_friend_request'] = $last_received_friend_request !== null ? $last_received_friend_request->attributes : null ;
+        $user_data['last_received_friend_request'] = $last_received_friend_request !== null ? $last_received_friend_request->attributes : null;
 
         return $user_data;
     }
 
     protected function _getUserPhotoUrl($link)
     {
-        if(strpos($link, 'upload') === 0){
-            return Url::base(true).'/'.$link;
-        }else{
+        if (strpos($link, 'upload') === 0) {
+            return Url::base(true) . '/' . $link;
+        } else {
             return $link;
         }
     }
@@ -225,9 +213,9 @@ class ApiBaseController extends Controller
     protected function _getLastFriendshipRequest($user_id, $friend_id)
     {
         $model = Friendship::find()
-                ->where(['user_id' => $user_id, 'friend_id' => $friend_id])
-                ->orderBy(['id' => SORT_DESC])
-                ->one();
+            ->where(['user_id' => $user_id, 'friend_id' => $friend_id])
+            ->orderBy(['id' => SORT_DESC])
+            ->one();
 
         return $model;
     }
@@ -235,7 +223,7 @@ class ApiBaseController extends Controller
     protected function _getCategories($parent_id = null)
     {
         $query = Category::find()
-                    ->where(['parent_id' => $parent_id]);
+            ->where(['parent_id' => $parent_id]);
         $model = $this->_getModelWithPagination($query);
 
         $categories = [];
@@ -243,9 +231,9 @@ class ApiBaseController extends Controller
             $temp['id'] = $category['id'];
             $temp['name'] = $category['name'];
             $temp['description'] = $category['description'];
-            $temp['main_image'] = Url::base(true).'/'.$category['main_image'];
-            $temp['icon'] = Url::base(true).'/'.$category['icon'];
-            $temp['badge'] = Url::base(true).'/'.$category['badge'];
+            $temp['main_image'] = Url::base(true) . '/' . $category['main_image'];
+            $temp['icon'] = Url::base(true) . '/' . $category['icon'];
+            $temp['badge'] = Url::base(true) . '/' . $category['badge'];
             $categories[] = $temp;
         }
 
@@ -255,8 +243,8 @@ class ApiBaseController extends Controller
     protected function _getBusinesses($conditions, $country_id = null, $order = null, $lat_lng = null, $andConditions = null)
     {
         $query = Business::find()
-                    ->where($conditions)
-                    ->with('category');
+            ->where($conditions)
+            ->with('category');
 
         if ($country_id !== null) {
             $query->andWhere(['country_id' => $country_id]);
@@ -264,7 +252,7 @@ class ApiBaseController extends Controller
 
         if ($order !== null) {
             $order = ['featured' => SORT_DESC] + $order;
-        }else{
+        } else {
             $order = ['featured' => SORT_DESC];
         }
 
@@ -273,7 +261,7 @@ class ApiBaseController extends Controller
             $lng = $lat_lng[1];
 
             $query
-                ->select(['*', '( 6371 * acos( cos( radians('.$lat.') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians('.$lng.') ) + sin( radians('.$lat.') ) * sin( radians( lat ) ) ) ) AS distance'])
+                ->select(['*', '( 6371 * acos( cos( radians(' . $lat . ') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(' . $lng . ') ) + sin( radians(' . $lat . ') ) * sin( radians( lat ) ) ) ) AS distance'])
                 ->having('distance < 100');
             $order += ['distance' => SORT_ASC];
         }
@@ -307,7 +295,7 @@ class ApiBaseController extends Controller
         $business['open_to'] = $model['open_to'];
         $business['lat'] = $model['lat'];
         $business['lng'] = $model['lng'];
-        $business['main_image'] = Url::base(true).'/'.$model['main_image'];
+        $business['main_image'] = Url::base(true) . '/' . $model['main_image'];
         $business['rating'] = $model['rating'];
         $business['price'] = $model['price'];
         $business['website'] = $model['website'];
@@ -325,7 +313,7 @@ class ApiBaseController extends Controller
         $business['no_of_checkins'] = count($model['checkins']);
         $business['no_of_reviews'] = count($model['reviews']);
         $business['last_checkin'] = null;
-        if (isset($model['checkins'][0])){
+        if (isset($model['checkins'][0])) {
             $last_checkin = $model['checkins'][0]->attributes;
             $last_checkin['user_data'] = $this->_getUserData($model['checkins'][0]->user);
             $business['last_checkin'] = $last_checkin;
@@ -341,9 +329,9 @@ class ApiBaseController extends Controller
     protected function _isSavedBusiness($user_id, $business_id)
     {
         $model = SavedBusiness::find()
-                    ->select('business_id')
-                    ->where(['user_id' => $user_id, 'business_id' => $business_id])
-                    ->one();
+            ->select('business_id')
+            ->where(['user_id' => $user_id, 'business_id' => $business_id])
+            ->one();
         return !empty($model);
     }
 
@@ -353,7 +341,7 @@ class ApiBaseController extends Controller
         $businessView->user_id = $user_id;
         $businessView->business_id = $business_id;
 
-        if(!$businessView->save()){
+        if (!$businessView->save()) {
             return $this->_getErrors($businessView); //saving problem
         }
 
@@ -363,10 +351,10 @@ class ApiBaseController extends Controller
     protected function _getCheckins($conditions)
     {
         $query = Checkin::find()
-                    ->where($conditions)
-                    ->orderBy(['id' => SORT_DESC])
-                    ->with('user')
-                    ->with('business');
+            ->where($conditions)
+            ->orderBy(['id' => SORT_DESC])
+            ->with('user')
+            ->with('business');
         $model = $this->_getModelWithPagination($query);
 
         $checkins = [];
@@ -396,9 +384,9 @@ class ApiBaseController extends Controller
     protected function _getReviews($conditions, $country_id = null)
     {
         $query = Review::find()
-                    ->where($conditions)
-                    ->orderBy(['id' => SORT_DESC])
-                    ->with('user');
+            ->where($conditions)
+            ->orderBy(['id' => SORT_DESC])
+            ->with('user');
 
         if ($country_id !== null) {
             $query
@@ -441,9 +429,9 @@ class ApiBaseController extends Controller
         $review_rating = $reviews->sum('rating');
         $review_no = count($reviews->all());
 
-        $total_rating = $checkin_rating+$review_rating;
-        $total_no = $checkin_no+$review_no;
-        $total_no = ($total_no==0)?1:$total_no;
+        $total_rating = $checkin_rating + $review_rating;
+        $total_no = $checkin_no + $review_no;
+        $total_no = ($total_no == 0) ? 1 : $total_no;
 
         return strval(round($total_rating / $total_no));
     }
@@ -451,9 +439,9 @@ class ApiBaseController extends Controller
     protected function _getMedia($conditions, $country_id = null)
     {
         $query = Media::find()
-                    ->where($conditions)
-                    ->orderBy(['id' => SORT_DESC])
-                    ->with('user');
+            ->where($conditions)
+            ->orderBy(['id' => SORT_DESC])
+            ->with('user');
 
         if ($country_id !== null) {
             $query
@@ -471,7 +459,7 @@ class ApiBaseController extends Controller
             }
 
             $temp['id'] = $value['id'];
-            $temp['url'] = Url::base(true).'/'.$value['url'];
+            $temp['url'] = Url::base(true) . '/' . $value['url'];
             $temp['type'] = $value['type'];
             $temp['object_id'] = $value['object_id'];
             $temp['object_type'] = $value['object_type'];
@@ -490,9 +478,9 @@ class ApiBaseController extends Controller
     {
         $media = new Media;
         $media->file = UploadedFile::getInstance($media, 'file');
-        if( isset($media->file) ){
-            $file_path = 'uploads/'.$media_type.'/'.$model_id.'.'.pathinfo($media->file->name, PATHINFO_EXTENSION);
-            while( file_exists($file_path) ){
+        if (isset($media->file)) {
+            $file_path = 'uploads/' . $media_type . '/' . $model_id . '.' . pathinfo($media->file->name, PATHINFO_EXTENSION);
+            while (file_exists($file_path)) {
                 $file_path = preg_replace('/(\.[^.]+)$/', sprintf('%s$1', '-'), $file_path);
             }
 
@@ -502,18 +490,18 @@ class ApiBaseController extends Controller
             $media->object_id = $model_id;
             $media->object_type = $object_type;
 
-            if($media->save()){
-                $media->file->saveAs($file_path);
-
-                if( !empty($model) && !empty($image_name) ){
-                    $model->$image_name = $file_path;
-
-                    if(!$model->save()){
-                        throw new HttpException(200, $this->_getErrors($user));
-                    }
-                }
-            }else{
+            if (!$media->save()) {
                 throw new HttpException(200, $this->_getErrors($media));
+            }
+
+            $media->file->saveAs($file_path);
+
+            if (!empty($model) && !empty($image_name)) {
+                $model->$image_name = $file_path;
+
+                if (!$model->save()) {
+                    throw new HttpException(200, $this->_getErrors($user));
+                }
             }
         }
     }
@@ -526,7 +514,7 @@ class ApiBaseController extends Controller
         $notification->body = $body;
         $notification->data = json_encode($data);
 
-        if(!$notification->save()){
+        if (!$notification->save()) {
             return $this->_getErrors($notification); //saving problem
         }
     }
