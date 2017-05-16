ebfea8f028e85963813f7d39c5f777d6bbc6dcbb
Unknown <mahmoud.gamal.mousa@gmail.com>
2017-05-16 23:03:24 +0800

    add cros support

--- config/web.php
+++ config/web.php
@@ -54,6 +54,15 @@ $config = [
             'enablePrettyUrl' => true,
             'baseUrl' => '/',
        ],
+        'corsFilter'  => [
+            'class' => \yii\filters\Cors::className(),
+            'cors'  => [
+                'Origin'                           => ['*'],
+                'Access-Control-Request-Method'    => ['POST'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age'           => 3600,
+            ],
+        ],
     ],
     'params' => $params,
 ];
