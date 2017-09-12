04a10b110168b7a22555c18d97cb5d2e2c6dd3a7
Mark Morcos <mark.yehia@gmail.com>
2017-09-12 11:44:05 +0200

    Increased max size of brochures

--- views/business/view.php
+++ views/business/view.php
@@ -155,7 +155,7 @@ span.interest {
                 'accept' => $type === 'brochure' ? 'application/pdf' : 'image/*'
             ],
             'clientOptions' => [
-                'maxFileSize' => 2000000
+                'maxFileSize' => $type === 'brochure' ? 5000000 : 2000000
             ],
             'clientEvents' => [
                 'fileuploaddone' => 'function(e, data) {
