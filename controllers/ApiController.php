e969dffa5612d04ac6de05d5c222abdd2719e319
Mark Morcos <mark.yehia@gmail.com>
2017-11-12 11:30:23 +0200

    Modified API docs for cigarettes

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1950,7 +1950,7 @@ class ApiController extends ApiBaseController
      * @apiParam {String} user_id User's id.
      * @apiParam {String} auth_key User's auth key.
      * @apiParam {String} business_id business's id to add media to.
-     * @apiParam {String} type Media's type (image, video, menu, product or brochure).
+     * @apiParam {String} type Media's type (image, video, menu, product, brochure or cigarette).
      * @apiParam {File} Media[file] Business's new file (optional).
      * @apiParam {String} caption Media's caption (optional).
      * @apiParam {String} rating Media's rating (optional).
