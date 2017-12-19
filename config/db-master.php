687ad72ce8c90e83100ca82093c66e8e71d2bfbd
Mahmoud Gamal <mahmoud.gamal.mousa@gmail.com>
2017-12-19 16:47:09 +0200

    Updated .gitignore

--- config/db-master.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=blabber_master',
-    'username' => 'blabber_user',
-    'password' => 'MMiP7gRIAFEX',
-    'charset' => 'utf8mb4',
-];
--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=blabber',
-    'username' => 'mark',
-    'password' => '',
-    'charset' => 'utf8mb4',
-];
